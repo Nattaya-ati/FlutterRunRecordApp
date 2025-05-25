963afac3b852bb888622e6787b82220737bb1fa7
Nattaya-ati <goodmorningsuncoffee@gmail.com>
2025-05-25 18:12:49 +0700

    ok V.1

---
+++ lib/models/run.dart
@@ -0,0 +1,30 @@
+//หน้าที่ของfile นี้คือ
+//1.เอาไว้แพ็คข้อมูล
+//2.เอาไว้แปลงข้อมูล JSON เพื่อส่งไปยัง API
+//3.เอาไว้แปลงข้อมูล JSON ที่ได้จาก API เป็นข้อมูลที่ใช้งานได้ใน APP
+// ignore_for_file: prefer_collection_literals, unnecessary_new, unnecessary_this
+
+class Run {
+  int? runId;
+  String? runLocation;
+  double? runDistance;
+  int? runTime;
+
+  Run({this.runId, this.runLocation, this.runDistance, this.runTime});
+
+  Run.fromJson(Map<String, dynamic> json) {
+    runId = json['runId'];
+    runLocation = json['runLocation'];
+    runDistance = double.parse(json['runDistance'].toString());
+    runTime = json['runTime'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['runId'] = this.runId;
+    data['runLocation'] = this.runLocation;
+    data['runDistance'] = this.runDistance;
+    data['runTime'] = this.runTime;
+    return data;
+  }
+}
